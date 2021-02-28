df797503ab1c4b5e154f73d37575e9f371a8fd3f
谢沛东 <peidongxie@outlook.com>
2021-02-28 18:07:07 +0800

    feat: 增加express错误示例接口

--- node-demo-express/index.js
+++ node-demo-express/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { Monitor, MonitorConfigProtocol } = require('@lite-monitor/node');
-
-const app = express();
-const port = 3001;
 const monitor = new Monitor({
   protocol: MonitorConfigProtocol.HTTP,
   host: 'localhost',
   port: 3000,
+});
+
+monitor.token = '3001300130013001';
+
+const app = express();
+
+app.get('/error', (req, res) => {
+  console.log(this());
 });
 
 app.get('/', (req, res) => {
@@ -15,4 +20,4 @@ app.get('/', (req, res) => {
 
 app.use(monitor.expressMiddleware());
 
-app.listen(port);
+app.listen(3001);
