7f3617ce56d6f6c5c25933fc709bb18a5e86cd08
谢沛东 <peidongxie@outlook.com>
2021-02-28 18:40:26 +0800

    feat: 实现koa demo

---
+++ node-demo-koa/index.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+const { Monitor, MonitorConfigProtocol } = require('@lite-monitor/node');
+
+const monitor = new Monitor({
+  protocol: MonitorConfigProtocol.HTTP,
+  host: 'localhost',
+  port: 3000,
+});
+monitor.token = '3002300230023002';
+
+const router = new Router();
+router.get('/error', (ctx) => {
+  this(ctx);
+});
+router.get('/', (ctx) => {
+  ctx.body = 'Hello World!';
+});
+
+const app = new Koa();
+app.use(monitor.koaMiddleware());
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(3002);
