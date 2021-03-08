129aa9ccea4c72df5326e37b35abcbe91a623da8
谢沛东 <peidongxie@outlook.com>
2021-03-08 21:18:29 +0800

    fix: 调整demo错误类型

--- web-demo-react/src/App.tsx
+++ web-demo-react/src/App.tsx
@@ -4,28 +4,31 @@ import './App.css';
 
 const App: FC<{ messages: string[] }> = ({ messages }) => {
   const message = messages[1];
-  const [hasError, setHasError] = useState(false);
+  const [syncsError, setSyncError] = useState(false);
+  const [asyncsError, setAsyncError] = useState(false);
+  if (syncsError) message.toLowerCase();
+  if (asyncsError) Promise.resolve().catch(() => message.toUpperCase());
+  const handleClickSyncError = useCallback(() => {
+    setSyncError(true);
+  }, []);
   const handleClickAsyncError = useCallback(() => {
-    fetch('https://localhost');
+    setAsyncError(true);
   }, []);
   const handleClickEventError = useCallback(() => {
-    message.toLowerCase();
+    message.toString();
   }, [message]);
-  const handleClickRenderError = useCallback(() => {
-    setHasError(true);
-  }, []);
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <span className='App-link' onClick={handleClickSyncError}>
+          {'Sync Error'}
+        </span>
         <span className='App-link' onClick={handleClickAsyncError}>
           {'Async Error'}
         </span>
         <span className='App-link' onClick={handleClickEventError}>
           {'Event Error'}
-        </span>
-        <span className='App-link' onClick={handleClickRenderError}>
-          {hasError ? message.toUpperCase() : 'Render Error'}
         </span>
       </header>
     </div>
