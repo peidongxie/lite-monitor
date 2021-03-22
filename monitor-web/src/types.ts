4a215bb3eaa7f419b58e785c0c76a7654b74dece
谢沛东 <peidongxie@outlook.com>
2021-03-23 00:52:37 +0800

    fix: 减少web监控产物大小

--- monitor-web/src/types.ts
+++ monitor-web/src/types.ts
@@ -4,19 +4,12 @@ export {
   AttrOs,
   AttrPlatform,
   AttrType,
-  MessageMethod,
-  MessageProtocol,
-  MessageVersion,
-  ResourceAction,
 } from '@lite-monitor/base';
 
 export type {
   ErrorEvent,
   Event,
   JsonItem,
-  MessageEvent,
   PrivateAttrs,
   PublicAttrs,
-  ResourceEvent,
-  ResourceSequenceElement,
 } from '@lite-monitor/base';
