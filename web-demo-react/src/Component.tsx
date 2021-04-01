e6e5811f61c8344d637a2f66a39341639664a3ed
谢沛东 <peidongxie@outlook.com>
2021-04-01 14:58:31 +0800

    feat: 上报组件事件

--- web-demo-react/src/Component.tsx
+++ web-demo-react/src/Component.tsx
@@ -9,31 +9,71 @@ import {
   PointerEventHandler,
   useCallback,
 } from 'react';
+import { ComponentAction, useMonitor } from '@lite-monitor/web';
 
 const Component: FC = () => {
+  const monitor = useMonitor();
   const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
-    (e) => console.log('Change', e.target.value, e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.CHANGE,
+        e.target.value,
+      ),
+    [monitor],
   );
   const handleClick = useCallback<MouseEventHandler<HTMLInputElement>>(
-    (e) => console.log('Click', e.detail, e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.CLICK,
+        String(e.detail),
+      ),
+    [monitor],
   );
   const handlePointerEnter = useCallback<PointerEventHandler<HTMLInputElement>>(
-    (e) => console.log('Enter', e.pointerType, e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.ENTER,
+        e.pointerType,
+      ),
+    [monitor],
   );
   const handlePointerOut = useCallback<PointerEventHandler<HTMLInputElement>>(
-    (e) => console.log('Out', e.pointerType, e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.OUT,
+        e.pointerType,
+      ),
+    [monitor],
   );
-  const handleDrag = useCallback<DragEventHandler<HTMLSpanElement>>((e) => {
-    e.dataTransfer.setData('text/plain', 'Hello World!');
-    console.log('Drag', e.dataTransfer.getData('text'), e);
-  }, []);
+  const handleDrag = useCallback<DragEventHandler<HTMLSpanElement>>(
+    (e) => {
+      e.dataTransfer.setData('text/plain', 'Hello World!');
+      monitor?.reportComponent(
+        'component-span-drag',
+        e.currentTarget,
+        ComponentAction.DRAG,
+        e.dataTransfer.getData('text'),
+      );
+    },
+    [monitor],
+  );
   const handleDrop = useCallback<DragEventHandler<HTMLSpanElement>>(
-    (e) => console.log('Drop', e.dataTransfer.getData('text'), e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-span-drop',
+        e.currentTarget,
+        ComponentAction.DROP,
+        e.dataTransfer.getData('text'),
+      ),
+    [monitor],
   );
   const handleKeyPress = useCallback<KeyboardEventHandler<HTMLInputElement>>(
     (e) => {
@@ -42,21 +82,44 @@ const Component: FC = () => {
         (ctrlKey ? 'ctrl+' : '') +
         (altKey ? 'alt+' : '') +
         (shiftKey ? 'shift+' : '');
-      console.log('Press', prefix + code, e);
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.PRESS,
+        prefix + code,
+      );
     },
-    [],
+    [monitor],
   );
   const handleCut = useCallback<ClipboardEventHandler<HTMLInputElement>>(
-    (e) => console.log('Cut', String(window.getSelection() || ''), e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.CUT,
+        String(window.getSelection() || ''),
+      ),
+    [monitor],
   );
   const handleCopy = useCallback<ClipboardEventHandler<HTMLInputElement>>(
-    (e) => console.log('Copy', String(window.getSelection() || ''), e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.COPY,
+        String(window.getSelection() || ''),
+      ),
+    [monitor],
   );
   const handlePaste = useCallback<ClipboardEventHandler<HTMLInputElement>>(
-    (e) => console.log('Paste', e.clipboardData.getData('text'), e),
-    [],
+    (e) =>
+      monitor?.reportComponent(
+        'component-input',
+        e.currentTarget,
+        ComponentAction.PASTE,
+        String(window.getSelection() || ''),
+      ),
+    [monitor],
   );
   return (
     <Fragment>
