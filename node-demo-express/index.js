72b5e03a139cd4993f9cd461f17d2a0e3d6a5222
谢沛东 <peidongxie@outlook.com>
2021-04-05 22:58:56 +0800

    feat: node示例增加cluster，上报资源事件

--- node-demo-express/index.js
+++ node-demo-express/index.js
@@ -1,5 +1,11 @@
+const cluster = require('cluster');
 const express = require('express');
-const { ExpressMonitor, MonitorConfigProtocol } = require('@lite-monitor/node');
+const os = require('os');
+const {
+  ExpressMonitor,
+  MonitorConfigProtocol,
+  ResourceAction,
+} = require('@lite-monitor/node');
 
 const config = {
   protocol: MonitorConfigProtocol.HTTP,
@@ -9,22 +15,63 @@ const config = {
 };
 const monitor = new ExpressMonitor(config);
 
-const messages = ['Hello World!'];
-const message = messages[1];
+if (cluster.isMaster) {
+  const count = Math.max(2, os.cpus().length);
+  for (let i = 0; i < count; i++) {
+    cluster.fork();
+  }
+  cluster.on('online', (worker) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.CREATE },
+    ]);
+  });
+  cluster.on('listening', (worker) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.START },
+    ]);
+  });
+  cluster.on('message', (worker, message) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.CONSUME, payload: 'message:' + message },
+    ]);
+    worker.send(`Hello Worker${worker.id}!`);
+  });
+  cluster.on('exit', (worker) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.STOP },
+      { action: ResourceAction.DESTROY },
+    ]);
+    cluster.fork();
+  });
+}
 
-const app = express();
-app.use(monitor.requestHandler);
-app.get('/error/sync', (req, res) => {
-  res.end('Sync Error');
-  message.toLowerCase();
-});
-app.get('/error/async', (req, res) => {
-  res.end('Async Error');
-  Promise.resolve().then(() => message.toUpperCase());
-});
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-app.all('*', monitor.defaultRouterHandler);
-app.use(monitor.errorRequestHandler);
-app.listen(3001);
+if (cluster.isWorker) {
+  const messages = ['Hello World!'];
+  const message = messages[1];
+  const app = express();
+  app.use(monitor.requestHandler);
+  app.get('/error/sync', (req, res) => {
+    res.end('Sync Error');
+    message.toLowerCase();
+  });
+  app.get('/error/async', (req, res) => {
+    res.end('Async Error');
+    Promise.resolve().then(() => message.toUpperCase());
+  });
+  app.get('/disconnect', (req, res) => {
+    res.end('Disconnect');
+    cluster.worker.disconnect();
+  });
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+    cluster.worker.send('Hello Master!');
+  });
+  app.all('*', monitor.defaultRouterHandler);
+  app.use(monitor.errorRequestHandler);
+  app.listen(3001);
+  cluster.worker.on('message', (message) => {
+    monitor.reportResource('worker' + cluster.worker.id, [
+      { action: ResourceAction.PRODUCE, payload: 'message:' + message },
+    ]);
+  });
+}
--- node-demo-koa/index.js
+++ node-demo-koa/index.js
@@ -1,6 +1,12 @@
+const cluster = require('cluster');
 const Koa = require('koa');
+const os = require('os');
 const Router = require('@koa/router');
-const { KoaMonitor, MonitorConfigProtocol } = require('@lite-monitor/node');
+const {
+  KoaMonitor,
+  MonitorConfigProtocol,
+  ResourceAction,
+} = require('@lite-monitor/node');
 
 const config = {
   protocol: MonitorConfigProtocol.HTTP,
@@ -10,24 +16,64 @@ const config = {
 };
 const monitor = new KoaMonitor(config);
 
-const messages = ['Hello World!'];
-const message = messages[1];
+if (cluster.isMaster) {
+  const count = Math.max(2, os.cpus().length);
+  for (let i = 0; i < count; i++) {
+    cluster.fork();
+  }
+  cluster.on('online', (worker) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.CREATE },
+    ]);
+  });
+  cluster.on('listening', (worker) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.START },
+    ]);
+  });
+  cluster.on('message', (worker, message) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.CONSUME, payload: 'message:' + message },
+    ]);
+    worker.send(`Hello Worker${worker.id}!`);
+  });
+  cluster.on('exit', (worker) => {
+    monitor.reportResource('worker' + worker.id, [
+      { action: ResourceAction.STOP },
+      { action: ResourceAction.DESTROY },
+    ]);
+    cluster.fork();
+  });
+}
 
-const router = new Router();
-router.get('/error/sync', (ctx) => {
-  ctx.body = 'Sync Error';
-  message.toLowerCase();
-});
-router.get('/error/async', (ctx) => {
-  ctx.body = 'Async Error';
-  Promise.resolve().then(() => message.toUpperCase());
-});
-router.get('/', (ctx) => {
-  ctx.body = 'Hello World!';
-});
-
-const app = new Koa();
-app.use(monitor.middleware);
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.listen(3002);
+if (cluster.isWorker) {
+  const messages = ['Hello World!'];
+  const message = messages[1];
+  const router = new Router();
+  router.get('/error/sync', (ctx) => {
+    ctx.body = 'Sync Error';
+    message.toLowerCase();
+  });
+  router.get('/error/async', (ctx) => {
+    ctx.body = 'Async Error';
+    Promise.resolve().then(() => message.toUpperCase());
+  });
+  router.get('/disconnect', (ctx) => {
+    ctx.body = 'Disconnect';
+    cluster.worker.disconnect();
+  });
+  router.get('/', (ctx) => {
+    ctx.body = 'Hello World!';
+    cluster.worker.send('Hello Master!');
+  });
+  const app = new Koa();
+  app.use(monitor.middleware);
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+  app.listen(3002);
+  cluster.worker.on('message', (message) => {
+    monitor.reportResource('worker' + cluster.worker.id, [
+      { action: ResourceAction.PRODUCE, payload: 'message:' + message },
+    ]);
+  });
+}
