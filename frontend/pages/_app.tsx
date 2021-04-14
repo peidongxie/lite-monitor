6880d49759a88fe41e3246d03403c0c69ac9e58b
谢沛东 <peidongxie@outlook.com>
2021-04-15 00:06:06 +0800

    fix: 修复服务端渲染造成的className不一致的问题

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,15 +5,13 @@ import Header from '../components/header';
 import '../styles/globals.css';
 
 const App: FC<AppProps> = (props) => {
-  console.log(props);
   const { Component, pageProps } = props;
   return (
     <Fragment>
-      <Header />
       <Head>
         <title>Lite Monitor</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Header />
       <Component {...pageProps} />
     </Fragment>
   );
