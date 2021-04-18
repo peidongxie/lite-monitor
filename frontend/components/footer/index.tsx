8aa8c81c5233a28a0b916713c1b9cc773799ba4a
谢沛东 <peidongxie@outlook.com>
2021-04-18 14:16:57 +0800

    feat: 加入底部链接

--- frontend/components/footer/index.tsx
+++ frontend/components/footer/index.tsx
@@ -1,5 +1,7 @@
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import { FC } from 'react';
 
 interface FooterProps {
@@ -16,22 +18,37 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
   },
   copyright: {
-    margin: theme.spacing(2.5, 0),
+    height: 48,
+    margin: theme.spacing(1, 0),
     color: theme.palette.grey[700],
     display: 'inline-block',
+  },
+  link: {
+    margin: '7px 8px 11px 8px',
   },
 }));
 
 const Footer: FC<FooterProps> = (props) => {
   const { year } = props;
   const classes = useStyles();
-  const theme = useTheme();
-  console.log(theme);
   return (
     <footer className={classes.root}>
-      <Typography className={classes.copyright} variant={'caption'}>
+      <Typography
+        className={classes.copyright}
+        color={'textSecondary'}
+        variant={'body2'}
+      >
         {'Copyright @ '}
         {year || new Date().getFullYear()}
+        <IconButton
+          className={classes.link}
+          color={'inherit'}
+          component={'a'}
+          href={'https://github.com/peidongxie/lite-monitor'}
+          size={'small'}
+        >
+          <GitHubIcon />
+        </IconButton>
       </Typography>
     </footer>
   );
