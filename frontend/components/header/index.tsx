8b4ad8735d134b9211e93455efc131bb10f051dc
谢沛东 <peidongxie@outlook.com>
2021-04-20 17:42:33 +0800

    feat: 顶导增加登录和多语言

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -1,44 +1,40 @@
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { FC } from 'react';
-import Logo from '../logo';
+import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
+import Label from '../label';
+import Lang from '../lang';
+import Login from '../login';
+import { Locale, useLocale } from '../../utils/locale';
 
 interface HeaderProps {
-  name: string;
+  setLocale: Dispatch<SetStateAction<Locale>>;
+  title: string;
 }
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     height: 64,
   },
-  logo: {},
-  name: {
-    fontWeight: 600,
-  },
+  label: {},
+  options: {},
   space: {
     flexGrow: 1,
   },
 }));
 
 const Header: FC<HeaderProps> = (props) => {
-  const { name } = props;
+  const { setLocale, title } = props;
+  const [options, setOptions] = useState([]);
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <AppBar color={'default'} position='static'>
         <Toolbar>
-          <IconButton className={classes.logo} color={'primary'}>
-            <Logo fontSize={'large'} />
-          </IconButton>
-          <Typography className={classes.name} color={'primary'} variant={'h6'}>
-            {name}
-          </Typography>
+          <Label className={classes.label} title={title} />
           <span className={classes.space}></span>
-          <Button color={'primary'}>登 录</Button>
+          <Login link={'/api/user/info'} />
+          <Lang setLocale={setLocale} />
         </Toolbar>
       </AppBar>
     </div>
