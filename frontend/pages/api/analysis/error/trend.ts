df8bbf91d9c4cea4f854a1a74edeade2109acac0
谢沛东 <peidongxie@outlook.com>
2021-05-21 19:05:28 +0800

    feat: 实现错误监控

--- frontend/pages/api/analysis/error/trend.ts
+++ frontend/pages/api/analysis/error/trend.ts
@@ -2,26 +2,26 @@ import { NextApiHandler } from 'next';
 
 interface Body {
   data: number[][];
-  label: string[];
+  labels: string[];
 }
 
 const getBody = (hour: number): Body => {
   const granularity = hour * 5 * 60 * 1000;
   const now = new Date().getTime();
   const end = now - (now % granularity);
-  const label: string[] = [];
+  const labels: string[] = [];
   for (let i = 0; i < 12; i++) {
     const date = new Date(end - i * granularity);
     const hour = date.getHours();
     const minute = date.getMinutes();
-    label.unshift(`${hour}:${minute < 10 ? '0' : ''}${minute}`);
+    labels.unshift(`${hour}:${String(minute).padStart(2, '0')}`);
   }
   return {
     data: [
       [0, 4, 7, 14, 24, 39, 56, 72, 84, 91, 96, 48],
       [0, 1, 2, 3, 6, 10, 14, 18, 21, 23, 24, 24],
     ],
-    label,
+    labels,
   };
 };
 
--- frontend/pages/project/error.tsx
+++ frontend/pages/project/error.tsx
@@ -1,18 +1,35 @@
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import { blue, red } from '@material-ui/core/colors';
+import { blue, grey, red } from '@material-ui/core/colors';
 import { makeStyles } from '@material-ui/core/styles';
-import { ChartConfiguration } from 'chart.js';
+import { ChartConfiguration, ChartDataset, ChartOptions } from 'chart.js';
 import { FC, Fragment, useMemo } from 'react';
 import useSWR from 'swr';
 import ChartBox from '../../components/chart-box';
+import CollapsibleTable from '../../components/collapsible-table';
 import SideDrawer from '../../components/side-drawer';
 import { jsonFetcher } from '../../utils/fetcher';
-import { useLocale } from '../../utils/locale';
+import { Locale, useLocale } from '../../utils/locale';
+import { format } from '../../utils/time';
 
 interface ErrorTrend {
   data: number[][];
-  label: string[];
+  labels: string[];
+}
+
+interface ErrorDetailItem {
+  name: string;
+  message: string;
+  stack: string[];
+  count: number;
+  items: {
+    timestamp: number;
+    user: string;
+    core: number;
+    memory: number;
+    platform: string;
+    platformVersion: string;
+  }[];
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -38,49 +55,191 @@ const useErrorTrend = (api: string) => {
 const config: ChartConfiguration<'line', number[], string> = {
   type: 'line',
   data: {
-    labels: ['', '', '', '', '', '', '', '', '', ''],
-    datasets: [
+    labels: [],
+    datasets: [],
+  },
+};
+
+const useData = (): number[][] => {
+  const errorTrend = useErrorTrend('/api/analysis/error/trend');
+  return errorTrend instanceof Object
+    ? errorTrend.data
+    : [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+};
+
+const useDatasets = (locale: Locale): ChartDataset<'line', number[]>[] => {
+  return useMemo<ChartDataset<'line', number[]>[]>(
+    () => [
       {
-        label: '错误总数',
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        borderColor: red[500],
         backgroundColor: red[500] + '50',
+        borderColor: red[500],
         cubicInterpolationMode: 'monotone',
+        data: [],
         fill: false,
+        label: (locale === 'zhCN' && '错误总数') || 'Error occurrences',
       },
       {
-        label: '影响人数',
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        borderColor: blue[500],
         backgroundColor: blue[500] + '50',
+        borderColor: blue[500],
         cubicInterpolationMode: 'monotone',
+        data: [],
         fill: true,
+        label: (locale === 'zhCN' && '影响人数') || 'Users affected',
       },
     ],
+    [locale],
+  );
+};
+
+const useLabels = (): string[] => {
+  const errorTrend = useErrorTrend('/api/analysis/error/trend');
+  return errorTrend instanceof Object
+    ? errorTrend.labels
+    : ['', '', '', '', '', '', '', '', '', ''];
+};
+
+const options: ChartOptions<'line'> = {
+  responsive: true,
+  interaction: {
+    intersect: false,
+    mode: 'index',
   },
-  options: {
-    responsive: true,
-    interaction: {
-      intersect: false,
-      mode: 'index',
-    },
-    scales: {
-      y: {
-        min: 0,
-        ticks: {
-          precision: 0,
-        },
+  scales: {
+    y: {
+      min: 0,
+      ticks: {
+        precision: 0,
       },
     },
   },
 };
 
-const defaultData = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
+const useChartBox = (locale: Locale): JSX.Element => {
+  return (
+    <ChartBox
+      config={config}
+      data={useData()}
+      datasets={useDatasets(locale)}
+      labels={useLabels()}
+      options={options}
+    />
+  );
+};
 
-const defaultLabel = ['', '', '', '', '', '', '', '', '', ''];
+const useErrorDetail = (api: string) => {
+  const { data, error } = useSWR<ErrorDetailItem[]>(api, jsonFetcher);
+  if (error) return typeof error === 'number' ? error : null;
+  return data;
+};
+
+const useBody = () => {
+  const errorDetail = useErrorDetail('/api/analysis/error/detail');
+  return useMemo(() => {
+    if (!Array.isArray(errorDetail)) return [];
+    return errorDetail.map((value) => ({
+      name: { children: value.name },
+      message: { children: value.message },
+      stack: { children: value.stack[value.stack.length - 1] },
+      count: { children: value.count },
+    }));
+  }, [errorDetail]);
+};
+
+const useCollapse = (locale: Locale) => {
+  const errorDetail = useErrorDetail('/api/analysis/error/detail');
+  const subbody = useMemo(() => {
+    if (!Array.isArray(errorDetail)) return [];
+    return errorDetail.map((value) => {
+      return value.items.map((value) => ({
+        timestamp: {
+          children: format(value.timestamp, 'yyyy-MM-dd HH:mm:ss.SSS'),
+        },
+        user: { children: value.user },
+        core: { children: value.core },
+        memory: { children: value.memory },
+        platform: { children: value.platform },
+        platformVersion: { children: value.platformVersion },
+      }));
+    });
+  }, [errorDetail]);
+  const subhead = useMemo(
+    () => [
+      {
+        key: 'timestamp',
+        children: (locale === 'zhCN' && '时间戳') || 'Timestamp',
+      },
+      {
+        key: 'user',
+        children: (locale === 'zhCN' && '用户标识') || 'User ID',
+      },
+      {
+        key: 'core',
+        children: (locale === 'zhCN' && '逻辑核心数') || 'Logical cores',
+      },
+      {
+        key: 'memory',
+        children: (locale === 'zhCN' && '内存总量') || 'Total memory (GB)',
+      },
+      {
+        key: 'platform',
+        children: (locale === 'zhCN' && '平台类型') || 'Platform type',
+      },
+      {
+        key: 'platformVersion',
+        children: (locale === 'zhCN' && '平台版本') || 'Platform version',
+      },
+    ],
+    [locale],
+  );
+  return useMemo(() => {
+    return subbody.map((value) => {
+      return (
+        <CollapsibleTable
+          body={value}
+          head={subhead}
+          tableProps={{ size: 'small', style: { backgroundColor: grey[200] } }}
+        />
+      );
+    });
+  }, [subbody, subhead]);
+};
+
+const useHead = (locale: Locale) => {
+  return useMemo(
+    () => [
+      {
+        key: 'name',
+        children: (locale === 'zhCN' && '错误名称') || 'Error name',
+      },
+      {
+        key: 'message',
+        children: (locale === 'zhCN' && '错误描述') || 'Error message',
+      },
+      {
+        key: 'stack',
+        children: (locale === 'zhCN' && '错误堆栈') || 'Error stack',
+      },
+      {
+        key: 'count',
+        children: (locale === 'zhCN' && '错误总数') || 'Error occurrences',
+      },
+    ],
+    [locale],
+  );
+};
+
+const useCollapsibleTable = (locale: Locale): JSX.Element => {
+  return (
+    <CollapsibleTable
+      body={useBody()}
+      collapse={useCollapse(locale)}
+      head={useHead(locale)}
+    />
+  );
+};
 
 const ErrorPage: FC = () => {
   const locale = useLocale();
@@ -111,20 +270,24 @@ const ErrorPage: FC = () => {
     ],
     [locale],
   );
-  const errorTrend = useErrorTrend('/api/analysis/error/trend');
-  const data = errorTrend instanceof Object ? errorTrend.data : defaultData;
-  const label = errorTrend instanceof Object ? errorTrend.label : defaultLabel;
+  const chartBox = useChartBox(locale);
+  const collapsibleTable = useCollapsibleTable(locale);
 
   return (
     <Fragment>
       <SideDrawer items={menuItems} />
       <Container maxWidth={false} className={classes.root}>
         <Container maxWidth={false} className={classes.trend}>
-          <Typography variant={'h6'}>错误趋势分析</Typography>
-          <ChartBox config={config} data={data} label={label} />
+          <Typography variant={'h6'}>
+            {(locale === 'zhCN' && '错误趋势分析') || 'Error Trend Analysis'}
+          </Typography>
+          {chartBox}
         </Container>
         <Container maxWidth={false} className={classes.detail}>
-          <Typography variant={'h6'}>错误详情分析</Typography>
+          <Typography variant={'h6'}>
+            {(locale === 'zhCN' && '错误详情分析') || 'Error Details Analysis'}
+          </Typography>
+          {collapsibleTable}
         </Container>
       </Container>
     </Fragment>
