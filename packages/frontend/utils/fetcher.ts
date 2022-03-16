cfa13587577d86cf3d49e7913ccfde16fd58cc93
谢沛东 <peidongxie@outlook.com>
2022-03-16 23:24:14 +0800

    feat: 实现表单 fetcher

--- packages/frontend/utils/fetcher.ts
+++ packages/frontend/utils/fetcher.ts
@@ -1,14 +1,37 @@
+const formFetcher = async <Body>(
+  url: string,
+  method?: string,
+  body?: FormData | URLSearchParams | null,
+  headers?: Record<string, string>,
+  init?: RequestInit,
+): Promise<Body> => {
+  const res = await fetch(url, {
+    method: method ?? 'POST',
+    body: body ?? null,
+    headers: {
+      Authorization: localStorage.getItem('token') || '',
+      ...headers,
+    },
+    ...init,
+  });
+  if (res.status >= 200 && res.status < 300) {
+    return res.json();
+  } else {
+    throw res.status;
+  }
+};
+
 const jsonFetcher = async <Body>(
   url: string,
   method?: string,
-  body?: BodyInit,
+  body?: unknown,
   headers?: Record<string, string>,
   init?: RequestInit,
 ): Promise<Body> => {
   const res = await fetch(url, {
     method: method ?? 'GET',
     body:
-      body === undefined
+      body === undefined || body === null
         ? null
         : JSON.stringify(body, (key, value) => {
             return typeof value === 'bigint' ? value.toString() + 'n' : value;
@@ -27,4 +50,4 @@ const jsonFetcher = async <Body>(
   }
 };
 
-export { jsonFetcher };
+export { formFetcher, jsonFetcher };
