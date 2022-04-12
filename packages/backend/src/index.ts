86c9fbdc9656a8807ce8ddf70b13c65b4aec36fe
谢沛东 <peidongxie@outlook.com>
2022-04-12 23:28:15 +0800

    feat: 优雅地关闭服务

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,3 +2,9 @@ import App from './app';
 
 const app = new App();
 await app.start();
+const shutdown = async () => {
+  await app.stop();
+  process.exit(0);
+};
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
