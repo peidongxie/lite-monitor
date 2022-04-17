c3d1dd02a3570bff0218d1e0c3f503c69d828f34
谢沛东 <peidongxie@outlook.com>
2022-04-17 22:24:11 +0800

    feat: 使用 react 18 createRoot

--- packages/web-react-demo/src/index.tsx
+++ packages/web-react-demo/src/index.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app';
 import './index.css';
 
-render(
+const container = globalThis.document.querySelector('#root');
+const root = container && createRoot(container);
+root?.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  globalThis.document.querySelector('#root'),
 );
