380ff0d5437a4ce66ac12333761370590127c57d
谢沛东 <peidongxie@outlook.com>
2023-03-19 23:56:36 +0800

    feat: 优化 demo 中错误事件示例

--- packages/web-react-demo/src/error.tsx
+++ packages/web-react-demo/src/error.tsx
@@ -7,37 +7,37 @@ import { ref } from './global';
 
 const Error: FC<{ messages: string[] }> = ({ messages }) => {
   const message = messages[1];
-  const [syncError, setSyncError] = useState(false);
-  const [asyncError, setAsyncError] = useState(false);
-  if (syncError) {
+  const [commonError, setCommonError] = useState(false);
+  const [uncaughtException, setUncaughtException] = useState(false);
+  const [unhandledRejection, setUnhandledRejection] = useState(false);
+  if (commonError) {
     message.toLowerCase();
   }
-  if (asyncError) {
-    setAsyncError(false);
-    // Report error events
-    const wrapped = getCallbackWithErrorCatch(fetch, ref);
-    wrapped('http://localhost:21');
+  if (uncaughtException) {
+    setTimeout(() => message.toString());
   }
-  const handleClickSyncError = useCallback(() => {
-    setSyncError(true);
+  if (unhandledRejection) {
+    Promise.resolve().then(() => message.toString());
+  }
+  const handleClickCommonError = useCallback(() => {
+    setCommonError(true);
   }, []);
-  const handleClickAsyncError = useCallback(() => {
-    setAsyncError(true);
+  const handleClickUncaughtException = useCallback(() => {
+    setUncaughtException(true);
   }, []);
-  // Report error events
-  const handleClickEventError = useCallbackWithErrorCatch(() => {
-    message.toUpperCase();
-  }, [message]);
+  const handleClickUnhandledRejection = useCallback(() => {
+    setUnhandledRejection(true);
+  }, []);
   return (
     <Fragment>
-      <span className={'app-link'} onClick={handleClickSyncError}>
-        {'Sync Error'}
+      <span className={'app-link'} onClick={handleClickCommonError}>
+        {'Common Error'}
       </span>
-      <span className={'app-link'} onClick={handleClickAsyncError}>
-        {'Async Error'}
+      <span className={'app-link'} onClick={handleClickUncaughtException}>
+        {'Uncaught Exception'}
       </span>
-      <span className={'app-link'} onClick={handleClickEventError}>
-        {'Event Error'}
+      <span className={'app-link'} onClick={handleClickUnhandledRejection}>
+        {'Unhandled Rejection'}
       </span>
     </Fragment>
   );
